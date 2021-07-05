0622461b48b95cb0492622b4975fb83872faa380
黑黑黑 <2270716426@qq.com>
2021-07-05 10:07:53 +0800

    liuliang的提交

--- hisbackstage/src/main/java/cn/gson/hisbackstage/HisbackstageApplication.java
+++ hisbackstage/src/main/java/cn/gson/hisbackstage/HisbackstageApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HisbackstageApplication {
 
     public static void main(String[] args) {
+        System.out.println("刘亮改了代码");
         SpringApplication.run(HisbackstageApplication.class, args);
     }
 
---
+++ hisbackstage/src/main/java/cn/gson/hisbackstage/mybatis/entity/User.java
@@ -0,0 +1,6 @@
+package cn.gson.hisbackstage.mybatis.entity;
+
+public class User {
+    private String name;
+    private String password;
+}
